b2dd80fad4a19947463f1ee70c497b3581cb1dd8
orthogonalmntn <orthogonalmntn@no.mail>
2022-02-27 15:03:30 +0100

    DRY up JWT verification

--- generator_service/jwt_service.py
+++ generator_service/jwt_service.py
@@ -3,10 +3,13 @@
 """""
 
 import jwt
-# from fastapi import HTTPException
+from fastapi import HTTPException
 
 def verify_token(token):
   secret_key = "SOME_SECRET_KEY"
   algorithm = "HS512"
 
-  jwt.decode(token, secret_key, algorithms=[algorithm])
+  try:
+    jwt.decode(token, secret_key, algorithms=[algorithm])
+  except jwt.exceptions.InvalidTokenError:
+    raise HTTPException(status_code=403, detail="Access denied")
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from typing import Optional
-from fastapi import FastAPI, Header, HTTPException
+from fastapi import FastAPI, Header
 
 from generator_service.planet_generator import generate_planet
 from generator_service.ship_generator import generate_ship
 from generator_service.shop_generator import generate_shop
 
 from generator_service.jwt_service import verify_token
-import jwt
+
 app = FastAPI()
 
 @app.get("/ping")
@@ -15,30 +15,21 @@ def ping():
 
 @app.get("/planet")
 def get_planet(x_token: Optional[str] = Header(None)):
-    try:
-      verify_token(x_token)
-    except jwt.exceptions.InvalidSignatureError:
-      raise HTTPException(status_code=403, detail="Access denied")
+    verify_token(x_token)
 
     generated_planet = generate_planet()
     return generated_planet
 
 @app.get("/ship")
 def get_ship(x_token: Optional[str] = Header(None)):
-    try:
-      verify_token(x_token)
-    except jwt.exceptions.InvalidSignatureError:
-      raise HTTPException(status_code=403, detail="Access denied")
+    verify_token(x_token)
 
     generated_ship = generate_ship()
     return generated_ship
 
 @app.get("/shop")
 def get_shop(x_token: Optional[str] = Header(None)):
-    try:
-      verify_token(x_token)
-    except jwt.exceptions.InvalidSignatureError:
-      raise HTTPException(status_code=403, detail="Access denied")
+    verify_token(x_token)
 
     generated_shop = generate_shop()
     return generated_shop
